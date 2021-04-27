4ddad1282b33fd34b642b19e534c6db9ac0e2d6b
Andrejs <ajancevskis@gmail.com>
2021-04-27 16:34:07 +0100

    attempt response fix

--- routes/account.js
+++ routes/account.js
@@ -4,7 +4,14 @@ const User = require("../models/User");
 
 router.get("/", verify, async (req, res, next) => {
   user = await User.findById({ _id: req.user._id });
-  res.send(user.name);
+  res.send({
+    user: {
+      id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+    },
+  });
   console.log(user);
 });
 
